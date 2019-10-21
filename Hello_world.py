329d4849e63a51a3c380c3753bda3d2e03ce2d03
yendrek78 <andrzej.stiller@gmial.com>
2019-10-21 19:50:46 +0200

    Zmiany w skryptach

--- Hello_world.py
+++ Hello_world.py
@@ -2,3 +2,5 @@
 #  Testowy skrypt na potrzeby Github
 #
 print("Ruda tańczy jak szalona!")
+a = 123.45
+print(a)
---
+++ napisy.py
@@ -0,0 +1,11 @@
+#
+# Typ danych: napisy (stringnapis1 = "")
+#
+napis1 = "ala ma kota"
+napis2 = 'ala ma kota'
+napis3 = 'a to jest "cudzysłów" '
+napis4 = "A to jest \"cudzysłów\" "
+napis5 = "Znaki specjalne: \t \n \r "
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis 1 to:", dlugosc, "znaków")
